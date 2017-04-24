0c7980032d3c14c849642327d79ee06a33c51a6b
Joe Hoyle <joehoyle@gmail.com>
2017-04-24 17:17:57 +0800

    switch to publically available site

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@
 import api from 'wordpress-rest-api-oauth-1'
 
 var config = {
-	url: 'http://localhost:8080/',
-	//url: 'https://demo.wp-api.org/',
+	//url: 'http://localhost:8080/',
+	url: 'https://demo.wp-api.org/',
 }
 
 if ( window.wpApiSettings ) {
