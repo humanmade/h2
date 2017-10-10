7c68c8dd59308850fae712fc0f1f09efd5586315
Ryan McCue <me@ryanmccue.info>
2017-10-10 08:07:44 +0400

    Focus editor when replying

--- src/components/WriteComment.js
+++ src/components/WriteComment.js
@@ -1,5 +1,7 @@
 // @flow
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './WriteComment.css';
 import '@humanmade/frontkit/dist/style.css';
 import type { User, Post, Comment } from '../types';
@@ -7,7 +9,7 @@ import Avatar from './Avatar';
 import Button from './Button';
 import FrontKit, { plugins } from '@humanmade/frontkit';
 
-export default function WriteComment(
+export default class WriteComment extends React.Component {
 	props: {
 		post: Post,
 		author: ?User,
@@ -15,33 +17,46 @@ export default function WriteComment(
 		onChange: Object => mixed,
 		onCancel: () => mixed,
 		onSave: () => mixed,
+	};
+
+	componentDidMount() {
+		if ( this.container && this.editor ) {
+			this.editor.focus();
+			const node = ReactDOM.findDOMNode( this.container );
+			if ( node && node.scrollIntoView ) {
+				node.scrollIntoView( false );
+			}
+		}
 	}
-) {
-	return (
-		<div className="WriteComment">
-			<Avatar
-				url={props.author ? props.author.avatar_urls['96'] : ''}
-				size={50}
-			/>
-			<div className="body">
-				<header>
-					<strong>{props.author ? props.author.name : ''}</strong>
-				</header>
-				<FrontKit
-					placeholder="Start writing..."
-					value={props.comment.content.edited}
-					onChange={edited => props.onChange({ content: { edited } })}
-				>
-					<plugins.Bubble />
-					<plugins.InlineBubble />
-					<plugins.Dots />
-					<plugins.MarkdownShortcuts />
-				</FrontKit>
-				<span className="buttons">
-					<Button onClick={props.onCancel}>Cancel</Button>
-					<Button onClick={props.onSave}>Save</Button>
-				</span>
+
+	render() {
+		return (
+			<div className="WriteComment" ref={ ref => this.container = ref }>
+				<Avatar
+					url={this.props.author ? this.props.author.avatar_urls['96'] : ''}
+					size={50}
+				/>
+				<div className="body">
+					<header>
+						<strong>{this.props.author ? this.props.author.name : ''}</strong>
+					</header>
+					<FrontKit
+						placeholder="Start writing..."
+						ref={ ref => this.editor = ref }
+						value={this.props.comment.content.edited}
+						onChange={edited => this.props.onChange({ content: { edited } })}
+					>
+						<plugins.Bubble />
+						<plugins.InlineBubble />
+						<plugins.Dots />
+						<plugins.MarkdownShortcuts />
+					</FrontKit>
+					<span className="buttons">
+						<Button onClick={this.props.onCancel}>Cancel</Button>
+						<Button onClick={this.props.onSave}>Save</Button>
+					</span>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
