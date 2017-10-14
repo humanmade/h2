e08a562de50a40cfc7c69bd7c441ff991807869d
Matthew Haines-Young <matthew@matth.eu>
2017-10-14 16:48:27 +0200

    Newline

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import './HeaderButton.css';
+
 export default function HeaderButton( props ) {
 	return <button onClick={props.onClick} className="HeaderButton">
 		{ props.icon && <span
