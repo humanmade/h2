8d4833191e4fd0b5d94ad6587047c05aab7ba01d
Matthew Haines-Young <matthew@matth.eu>
2017-10-14 22:02:54 +0200

    Emoji reactions

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,35 +1,55 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import { FormattedRelative } from 'react-intl';
 
 import Avatar from './Avatar';
 import Button from './Button';
 import PostContent from './PostContent'
+import Reactions from '../containers/Reactions'
 import { Post as PostType, User } from '../shapes';
 
 import './Post.css';
 
-export default function Post( props ) {
-	return <div className="Post">
-		<header>
-			<Avatar
-				url={props.author ? props.author.avatar_urls['96'] : ''}
-				size={70}
-			/>
-			<div className="byline">
-				<h2 dangerouslySetInnerHTML={{ __html: props.post.title.rendered }} />
-				<div className="date">
-					{props.author.name},&nbsp;
-					<FormattedRelative value={props.post.date_gmt} />
+export default class Post extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			reactions: {}
+		}
+	}
+
+	render() {
+		const props = this.props;
+
+		return <div className="Post">
+			<header>
+				<Avatar
+					url={props.author ? props.author.avatar_urls['96'] : ''}
+					size={70}
+				/>
+				<div className="byline">
+					<h2 dangerouslySetInnerHTML={{ __html: props.post.title.rendered }} />
+					<div className="date">
+						{props.author.name},&nbsp;
+						<FormattedRelative value={props.post.date_gmt} />
+					</div>
 				</div>
-			</div>
-			<div className="actions">
-				<Button onClick={props.onComment}>Reply</Button>
-			</div>
-		</header>
-		<PostContent html={props.post.content.rendered} />
-		{props.children}
-	</div>;
+				<div className="actions">
+					<Button onClick={props.onComment}>Reply</Button>
+				</div>
+			</header>
+			<PostContent html={props.post.content.rendered} />
+			{props.children}
+
+			<Reactions
+				reactions={ this.state.reactions }
+				onChangeReactions={ newReactions => {
+					this.setState({ reactions: newReactions })
+				} }
+			/>
+		</div>;
+	}
 }
 
 Post.propTypes = {
---
+++ src/components/Reactions.js
@@ -0,0 +1,87 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Picker } from 'emoji-mart'
+
+import 'emoji-mart/css/emoji-mart.css';
+import './Reactions.css';
+
+export default class Reaction extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			isOpen: false
+		}
+	}
+
+	render() {
+		const { reactions, userId } = this.props;
+
+		if ( ! userId || userId <= 0 ) {
+			return null;
+		}
+
+		return <div className="reactions">
+			<button
+				className="btn btn--small btn--tertiary"
+				onClick={ value => this.setState({ isOpen: ! this.state.isOpen  } ) }
+				key="button"
+			>
+				Add reaction
+			</button>
+			<div class="reactions" key="reactions">
+				{ Object.entries( reactions ).map( ( [ emoji, count ] ) => {
+					let isActive = reactions[ emoji ].indexOf( userId ) >= 0 ? true : false;
+					return <button
+						className={ 'btn btn--small btn--tertiary' + ( isActive ? ' btn--active' : '' ) }
+						onClick={ () => this.toggleReaction( emoji ) }
+						key={ emoji }
+					>
+						<span className="reactions__emoji">{ emoji }</span>
+						<span className="reactions__count">{ count.length }</span>
+					</button>
+				})}
+			</div>
+			{ this.state.isOpen && (
+				<Picker
+					key="picker"
+					onClick={ data => {
+						this.setState( { isOpen: false } );
+						this.toggleReaction( data.native );
+					}}
+				/>
+			)}
+		</div>;
+	}
+
+	toggleReaction( emoji ) {
+		const { reactions, onChangeReactions, userId } = this.props;
+
+		if ( ! ( emoji in reactions ) ) {
+			reactions[ emoji ] = [ userId ];
+		} else if ( reactions[ emoji ].indexOf( userId ) < 0 ) {
+			reactions[ emoji ].push( userId );
+		} else {
+			let index = reactions[ emoji ].indexOf( userId );
+			if ( index > -1 ) {
+	 			reactions[ emoji ].splice( index, 1 );
+			}
+
+			if ( reactions[ emoji ].length < 1 ) {
+				delete reactions[ emoji ];
+			}
+		}
+
+		onChangeReactions( reactions );
+	}
+}
+
+
+Reaction.propTypes = {
+	onChangeReactions: PropTypes.func.isRequired,
+	reactions: PropTypes.object.isRequired,
+};
+
+Reaction.defaultProps = {
+	userId: 0,
+}
