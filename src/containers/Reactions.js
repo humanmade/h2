df7a3a2793426ba460c40c81383410114fbc8500
Ignacio Cruz <igmoweb@gmail.com>
2017-10-18 17:10:23 +0200

    Fixed indentation

--- src/containers/Reactions.js
+++ src/containers/Reactions.js
@@ -2,8 +2,8 @@ import { connect } from 'react-redux';
 import Reactions from '../components/Reactions';
 
 const mapStateToProps = state => {
-    let user = state.user.byId[ Object.keys( state.user.byId )[ 0 ] ];
-    return { userId: typeof user !== 'undefined' ? user.id : 0 }
+	let user = state.user.byId[ Object.keys( state.user.byId )[ 0 ] ];
+	return { userId: typeof user !== 'undefined' ? user.id : 0 }
 };
 
 export default connect( mapStateToProps )( Reactions );
