7ba38694af379b3b1d5bb364cde021151d550cd8
Matthew Haines-Young <matthew@matth.eu>
2017-10-19 16:25:50 +0100

    Error handling

--- src/wp-api-redux/createDeleteObjectActionCreator.js
+++ src/wp-api-redux/createDeleteObjectActionCreator.js
@@ -1,24 +1,33 @@
 export default function createFetchObjectActionCreator( objectName, path, api ) {
 	return function actionCreator( objectId ) {
-		return ( disaptch, getStore ) => {
-			disaptch( {
+		return ( dispatch, getStore ) => {
+			dispatch( {
 				type:    `WP_API_REDUX_DELETE_${ objectName.toUpperCase() }_UPDATING`,
 				payload: {
 					objectName,
 					objectId,
 				},
 			} );
-			return api.del( `${path}/${objectId}` ).then( object => {
-				disaptch( {
-					type:    `WP_API_REDUX_DELETE_${ objectName.toUpperCase() }_UPDATED`,
-					payload: {
-						objectName,
-						object,
-						objectId,
-					},
+			return api
+				.del( `${path}/${objectId}` )
+				.then( object => {
+					dispatch( {
+						type:    `WP_API_REDUX_DELETE_${ objectName.toUpperCase() }_UPDATED`,
+						payload: {
+							objectName,
+							object,
+							objectId,
+						},
+					} );
+					return object;
+				} )
+				.catch( error => {
+					dispatch( {
+						type:    `WP_API_REDUX_DELETE_${objectName.toUpperCase()}_ERRORED`,
+						payload: { error, objectId },
+					} );
+					throw error;
 				} );
-				return object;
-			} );
 		};
 	};
 }
