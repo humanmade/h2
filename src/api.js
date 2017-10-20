8b39211072465aef808365b56fd80d10844b90e3
Joe Hoyle <joehoyle@gmail.com>
2017-10-20 22:02:12 +0300

    Switch to OAuth 2

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-import api from 'wordpress-rest-api-oauth-1';
+import api from './lib/wordpress-rest-api-oauth-2/src/';
 
 const url = window.location.href.split( '?' )[0];
 let config = {
@@ -8,28 +8,9 @@ let config = {
 };
 
 if ( process.env.NODE_ENV === 'development' ) {
-	config.credentials = {
-		client: {
-			public: 'r00m8n0sl4G4',
-			secret: 'KqUYmZ211O9ie6JR47QMa24A4xeV82hhDLJp33dTzwBFUwpG',
-		},
-	};
+	config.credentials = { client: { id: 'ixgtsngf5p5o' } };
 } else if ( config.url === 'https://updates.hmn.md/' ) {
-	config.credentials = {
-		client: {
-			public: '1ndRXh0MJMg6',
-			secret: 'krPrIswDMRhmjWih0kpDm4rgZ0w0V69MzpCX4f0JhJehYRrR',
-		},
-	};
-}
-
-if ( config.url === 'http://localhost:8080/' ) {
-	config.credentials = {
-		client: {
-			public: 'NAqxuXe8BDtl',
-			secret: 'SQ4ZJOatH79swrAGRmyqFcJ6qBZJhtMV5aMdTgI1OPo8yxN0',
-		},
-	};
+	config.credentials = { client: { id: 'b73rv7has0q2' } };
 }
 
 if ( window.wpApiSettings ) {
