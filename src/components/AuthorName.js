3403cbf7ee62191d7134e4dca24d670efbf7ecb3
Ryan McCue <me@ryanmccue.info>
2017-10-29 22:34:40 +0200

    Add hovercards for authors

--- src/components/AuthorName.js
+++ src/components/AuthorName.js
@@ -1,12 +1,39 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Avatar from './Avatar';
+import Hovercard from './Hovercard';
+
+import './AuthorName.css';
+
 export default class AuthorName extends React.Component {
+	renderCard() {
+		const { user } = this.props;
+
+		return <aside className="AuthorName-Card">
+			<Avatar
+				url={ user.avatar_urls['96'] }
+				size={ 40 }
+			/>
+			<div className="AuthorName-Card-details">
+				<h1>{ user.name }</h1>
+				<div className="AuthorName-Card-description">
+					{ user.description }
+				</div>
+			</div>
+		</aside>;
+	}
+
 	render() {
 		const { user } = this.props;
-		return <span className="author-name">
-			{ user.name }
-		</span>;
+		return <Hovercard
+			cardContent={ () => this.renderCard() }
+			width={ 500 }
+		>
+			<span className="author-name">
+				{ user.name }
+			</span>
+		</Hovercard>;
 	}
 }
 
