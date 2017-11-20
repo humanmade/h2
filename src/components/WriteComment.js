59a92b3d0127f0735715a7d34f107e1284833990
Ryan McCue <me@ryanmccue.info>
2017-11-20 16:31:38 +1000

    Add autosave support to comments

--- src/components/WriteComment.js
+++ src/components/WriteComment.js
@@ -31,6 +31,7 @@ export default class WriteComment extends React.Component {
 			<div className="body">
 				<Editor
 					ref={editor => this.editor = editor ? editor.getWrappedInstance() : null}
+					defaultValue={ this.props.autosave }
 					onAutosave={ this.props.onAutosave }
 					onCancel={this.props.onCancel}
 					onSubmit={this.props.onSave}
@@ -43,6 +44,7 @@ export default class WriteComment extends React.Component {
 
 WriteComment.propTypes = {
 	author:     User,
+	autosave:   PropTypes.string,
 	comment:    Comment.isRequired,
 	post:       Post.isRequired,
 	onAutosave: PropTypes.func.isRequired,
--- src/containers/WriteComment.js
+++ src/containers/WriteComment.js
@@ -7,13 +7,33 @@ import WriteComment from '../components/WriteComment';
 import { Post, PostsState, UsersState, Comment, Dispatch } from '../shapes';
 import store from '../store';
 
+const DRAFT_PREFIX = 'h2-draft-comment-';
+
 class ConnectedWriteComment extends Component {
+	componentWillMount() {
+		const autosave = window.localStorage.getItem( this.getDraftKey() ) || '';
+		this.setState( { autosave } );
+	}
+
+	getDraftKey() {
+		return DRAFT_PREFIX + this.props.post.id;
+	}
+
+	onAutosave( content ) {
+		// Save to localStorage asynchronously.
+		window.setTimeout( () => {
+			window.localStorage.setItem( this.getDraftKey(), content );
+		} );
+	}
+
 	onCancel() {
 		this.props.dispatch( {
 			type:    'WRITE_COMMENT_CANCELLED',
 			payload: { postId: this.props.post.id, comment: this.props.comment },
 		} );
+		window.localStorage.removeItem( this.getDraftKey() );
 	}
+
 	onSave( content ) {
 		const newComment = {
 			content,
@@ -21,7 +41,9 @@ class ConnectedWriteComment extends Component {
 			post:   this.props.comment.post,
 		};
 		this.props.dispatch( store.actions.comments.create( newComment ) );
+		window.localStorage.removeItem( this.getDraftKey() );
 	}
+
 	onChange( comment ) {
 		this.props.dispatch( {
 			type:    'WRITE_COMMENT_UPDATED',
@@ -31,16 +53,20 @@ class ConnectedWriteComment extends Component {
 			},
 		} );
 	}
+
 	onUpload( file ) {
 		return this.props.dispatch( uploadMedia( file ) );
 	}
+
 	render() {
 		const post = this.props.post;
 		const author = Object.values( this.props.user.byId )[0];
 		return <WriteComment
 			author={author}
+			autosave={ this.state.autosave }
 			comment={this.props.comment}
 			post={post}
+			onAutosave={ content => this.onAutosave( content ) }
 			onCancel={() => this.onCancel()}
 			onChange={comment => this.onChange( comment )}
 			onSave={ comment => this.onSave( comment ) }
