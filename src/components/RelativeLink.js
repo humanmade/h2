1667209dd0a92384458d1f5ec2d1a1705d15cd65
Joe Hoyle <joehoyle@gmail.com>
2018-01-25 18:07:33 -0600

    Fix component name

--- src/components/RelativeLink.js
+++ src/components/RelativeLink.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function InternalLink( { to, children } ) {
+export default function RelativeLink( { to, children } ) {
 	return <Link to={ to.replace( /^(?:\/\/|[^/]+)*\//, '/' ) }>{ children }</Link>
 }
