acf028cd8f6cda88900eb46d1b234deb87c04979
Ryan McCue <me@ryanmccue.info>
2018-03-12 12:39:52 +1000

    Fix coding standards issues

--- src/reducers.js
+++ src/reducers.js
@@ -2,6 +2,4 @@ import { combineReducers } from 'redux';
 
 import { reducer as plugins } from './plugins';
 
-export default combineReducers( {
-	plugins,
-} );
+export default combineReducers( { plugins } );
