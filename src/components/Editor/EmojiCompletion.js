fe7c36319bfbdb6b4812c1fb22eb11a5681b809a
Ryan McCue <me@ryanmccue.info>
2018-04-16 12:07:10 +1000

    Use custom emoji in autocomplete

--- src/components/Editor/EmojiCompletion.js
+++ src/components/Editor/EmojiCompletion.js
@@ -3,21 +3,36 @@ import React from 'react';
 
 import Completion from './Completion';
 
+import './EmojiCompletion.css';
+
+const emoji = [
+	...Object.values( emojiIndex.emojis ),
+	...window.H2Data.site.emoji,
+];
+
 const EmojiCompletion = props => {
 	const renderItem = ( { item, selected, onSelect } ) => {
 		return <li
 			key={ item.colons }
 			className={ selected ? 'selected' : null }
 			onClick={ onSelect }
-		>{ item.native } { item.colons }</li>;
+		>
+			{ item.imageUrl ? (
+				<img
+					className="EmojiCompletion-custom"
+					src={ item.imageUrl }
+				/>
+			) : item.native }
+			{ item.colons }
+		</li>;
 	};
 
 	return <Completion
 		{ ...props }
 		matcher={ ( item, search ) => `${ item.colons } ${ item.name }`.toLowerCase().indexOf( search.toLowerCase() ) >= 0 }
-		items={ Object.values( emojiIndex.emojis ) }
+		items={ emoji }
 		renderItem={ renderItem }
-		insert={ item => item.native + ' ' }
+		insert={ item => ( item.native || item.colons ) + ' ' }
 	/>;
 };
 
