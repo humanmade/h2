9635570d8eecede0f8a40bfffb15bf8955ab5ce7
Ryan McCue <me@ryanmccue.info>
2018-04-18 17:08:12 +1000

    Use Interweave to parse HTML content

--- src/components/Message/Content.js
+++ src/components/Message/Content.js
@@ -1,13 +1,17 @@
+import Interweave from 'interweave';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import './Content.css';
 
 export default function Content( props ) {
-	return <div
-		className="PostContent"
-		dangerouslySetInnerHTML={{ __html: props.html }}
-	/>;
+	return <div className="PostContent">
+		<Interweave
+			commonClass={ null }
+			content={ props.html }
+			tagName="fragment"
+		/>
+	</div>;
 }
 
 Content.propTypes = { html: PropTypes.string.isRequired };
