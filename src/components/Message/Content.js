3e349b256aba4f5f3ab55ee935b2701d5961e04b
Ryan McCue <me@ryanmccue.info>
2018-04-18 17:08:46 +1000

    Parse out usernames in mentions

--- src/components/Message/Content.js
+++ src/components/Message/Content.js
@@ -2,13 +2,20 @@ import Interweave from 'interweave';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { MentionMatcher } from './Mention';
+
 import './Content.css';
+
+const MATCHERS = [
+	new MentionMatcher( 'mention' ),
+];
 
 export default function Content( props ) {
 	return <div className="PostContent">
 		<Interweave
 			commonClass={ null }
 			content={ props.html }
+			matchers={ MATCHERS }
 			tagName="fragment"
 		/>
 	</div>;
---
+++ src/components/Message/Mention.js
@@ -0,0 +1,47 @@
+import { Matcher } from 'interweave';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import AuthorLink from './AuthorLink';
+import { withApiData } from '../../with-api-data';
+
+class Mention extends React.Component {
+	render() {
+		const user = this.props.user.data ? this.props.user.data[0] : null;
+
+		return <AuthorLink user={ user }>
+			{ this.props.children }
+		</AuthorLink>;
+	}
+}
+
+Mention.propTypes = {
+	user: PropTypes.object,
+};
+
+const ConnectedMention = withApiData( props => ( { user: `/wp/v2/users?slug=${ props.username }` } ) )( Mention );
+
+export default ConnectedMention;
+
+export class MentionMatcher extends Matcher {
+	replaceWith( match, props ) {
+		return <ConnectedMention
+			key={ props.key }
+			username={ props.username }
+		>
+			{ match }
+		</ConnectedMention>;
+	}
+
+	asTag(): string {
+		return 'span';
+	}
+
+	match( string ) {
+		return this.doMatch( string, /@(\w+)/, matches => {
+			return {
+				username: matches[1],
+			};
+		} );
+	}
+}
