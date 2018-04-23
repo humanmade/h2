a9f90bb3c742d7fcc8b2a15b8f43c86def139779
Ryan McCue <me@ryanmccue.info>
2018-04-23 17:35:43 +1000

    Trigger meta sidebar on click

--- src/components/Header/CurrentUser.js
+++ src/components/Header/CurrentUser.js
@@ -1,41 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import Dropdown from './Dropdown';
 import Avatar from '../Avatar';
-import Button from '../Button';
+import { showMetaSidebar } from '../../actions';
 import { User } from '../../shapes';
 
 import './CurrentUser.css';
 
-export default function CurrentUser( props ) {
+function CurrentUser( props ) {
 	const { user } = props;
-	const label = <Avatar
-		key="avatar"
-		size={ 40 }
-		url={ user.avatar_urls['96'] }
-	/>;
 
-	return <Dropdown
+	return <button
 		className="Header-CurrentUser"
-		label={ label }
+		onClick={ props.onShowSidebar }
 	>
 		<Avatar
 			key="avatar"
-			size={ 96 }
+			size={ 40 }
 			url={ user.avatar_urls['96'] }
 		/>
-		<div>
-			<p className="Header-CurrentUser-name">{ user.name }</p>
-			<p className="Header-CurrentUser-username"><code>@{ user.slug }</code></p>
-			<ul className="Header-CurrentUser-items">
-				<li>
-					<Button
-						onClick={ props.onLogOut }
-					>Log out</Button>
-				</li>
-			</ul>
-		</div>
-	</Dropdown>;
+	</button>;
 }
 
 CurrentUser.propTypes = { user: User.isRequired };
+
+const mapDispatchToProps = dispatch => {
+	return { onShowSidebar: () => dispatch( showMetaSidebar() ) };
+};
+
+export default connect( () => ( {} ), mapDispatchToProps )( CurrentUser );
