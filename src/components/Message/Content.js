23c969df92d70b4ba963f012a9c88198a41f88c6
Ryan McCue <me@ryanmccue.info>
2018-04-24 15:25:42 +1000

    Render tasklists using the component

--- src/components/Message/Content.js
+++ src/components/Message/Content.js
@@ -1,14 +1,64 @@
+import Tasklist from '@humanmade/react-tasklist';
 import Interweave from 'interweave';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import { MentionMatcher } from './Mention';
 
+import '@humanmade/react-tasklist/css/index.css';
 import './Content.css';
 
 const MATCHERS = [
 	new MentionMatcher( 'mention' ),
 ];
+
+class DummyComponent extends React.Component {
+	render() {
+		return <li className="Tasklist-Item">{ this.props.children }</li>;
+	}
+}
+
+const transform = ( node, children, config ) => {
+	if ( node.tagName === 'LI' && node.className === 'Tasklist-Item' ) {
+		// If it's a tasklist item, wrap in a dummy component and return,
+		// allowing us to use the children later.
+		return <DummyComponent
+			checked={ 'checked' in node.dataset }
+			children={ children }
+			config={ config }
+			node={ node }
+		/>;
+	}
+
+	if ( node.tagName === 'UL' && node.className === 'Tasklist' ) {
+		// Parse out our tasks.
+		const items = children.map( child => {
+			if ( ! React.isValidElement( child ) || typeof child !== 'object' ) {
+				return null;
+			}
+
+			if ( child.type !== DummyComponent ) {
+				return {
+					id:    child.key,
+					label: child,
+					task:  false,
+				};
+			}
+
+			return {
+				id:      child.key,
+				label:   <React.Fragment>{ child.props.children }</React.Fragment>,
+				checked: child.props.checked,
+			};
+		} ).filter( Boolean );
+
+		return <Tasklist
+			items={ items }
+			onChange={ ( ...args ) => console.log( args ) }
+			onReorder={ ( ...args ) => console.log( args ) }
+		/>;
+	}
+};
 
 export default function Content( props ) {
 	return <div className="PostContent">
@@ -17,6 +67,7 @@ export default function Content( props ) {
 			content={ props.html }
 			matchers={ MATCHERS }
 			tagName="fragment"
+			transform={ transform }
 		/>
 	</div>;
 }
