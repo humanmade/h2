47861f0d01aa72759c5f0ab7702b23b8461621f6
Ryan McCue <me@ryanmccue.info>
2018-05-20 10:48:37 +0100

    Split changelog to separate file

--- src/components/Changes.js
+++ src/components/Changes.js
@@ -1,63 +1,13 @@
 import React from 'react';
 
 import Button from './Button';
-import AuthorLink from './Message/AuthorLink';
+import { changes, getChangesForUser } from '../changelog';
 import { withApiData } from '../with-api-data';
 
 import './Changes.css';
 
-const changes = [
-	// Add new changes to the bottom of this list, in the following format:
-	{
-		date: '2018-02-26',
-		title: 'See What\'s New',
-		content: () => <React.Fragment>
-			<p>H2 now includes a changelog (you're looking at it!) to let you know of any new features.</p>
-			<p>(We'll only use this for new major features; keep an eye on the <a href="https://github.com/humanmade/H2">H2 repo</a> if you want to see minor changes too!)</p>
-		</React.Fragment>,
-	},
-	{
-		date: '2018-03-06',
-		title: 'React to Comments',
-		content: () => <p>You can now <span role="img" aria-label="">🎉</span> emoji react to <span role="img" aria-label="">💬</span> comments. <span role="img" aria-label="">🙌</span></p>,
-	},
-	{
-		date: '2018-03-26',
-		title: 'More Useful Hovercards',
-		content: withApiData( props => ( { user: '/wp/v2/users/me' } ) )( props => <React.Fragment>
-			<p>Hovercards are now more useful, and will be displayed on usernames and avatars.</p>
-			{ props.user.isLoading ? (
-				<p>For example, hover over your name to see yours: <em>loading…</em></p>
-			) : (
-				<p>For example, hover over your name to see yours: <AuthorLink user={ props.user.data }>{ props.user.data.name }</AuthorLink></p>
-			) }
-			<p>You can also click linked names to show a full profile.</p>
-		</React.Fragment> ),
-	},
-	{
-		date: '2018-04-16',
-		title: 'Custom Emoji',
-		content: () => <p>
-			You can now use Slack custom emoji in posts or reactions.
-
-			{ ' ' }
-
-			{ window.H2Data.site.emoji['mindblown'] ? (
-				<img
-					alt=":mindblown:"
-					src={ window.H2Data.site.emoji['mindblown'].imageUrl }
-				/>
-			) : null }
-		</p>,
-	},
-];
-
 function Changes( props ) {
-	const { lastView, onDismiss } = props;
-	const newChanges = changes.filter( change => {
-		const changeDate = new Date( change.date );
-		return changeDate > lastView;
-	} );
+	const { newChanges, onDismiss } = props;
 	if ( ! newChanges.length ) {
 		return null;
 	}
@@ -94,7 +44,7 @@ class ConnectedChanges extends React.Component {
 	render() {
 		if ( this.props.forceShow ) {
 			return <Changes
-				lastView={ new Date( '1970-01-01' ) }
+				newChanges={ changes }
 				onDismiss={ this.props.onDismiss }
 			/>;
 		}
@@ -103,8 +53,7 @@ class ConnectedChanges extends React.Component {
 			return null;
 		}
 
-		const rawLastView = this.props.currentUser.data.meta.h2_last_updated || '1970-01-01T00:00:00';
-		const lastView = new Date( rawLastView + 'Z' );
+		const newChanges = getChangesForUser( this.props.currentUser.data );
 
 		const onDismiss = () => {
 			const meta = { h2_last_updated: ( new Date() ).toISOString() };
@@ -118,7 +67,7 @@ class ConnectedChanges extends React.Component {
 		}
 
 		return <Changes
-			lastView={ lastView }
+			newChanges={ newChanges }
 			onDismiss={ onDismiss }
 		/>;
 	}
