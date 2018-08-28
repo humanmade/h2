8d41260d8d90f6c68352ff1ecf70a89edf90f673
Ryan McCue <me@ryanmccue.info>
2018-08-28 13:37:08 +1000

    Add error boundary to content renderer

--- src/components/Message/Content.js
+++ src/components/Message/Content.js
@@ -3,9 +3,37 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
+import Notification from '../Notification';
 import matchers from '../../matchers';
 
 import './Content.css';
+
+class ErrorBoundary extends React.Component {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			error: false,
+		};
+	}
+
+	componentDidCatch( error, info ) {
+		this.setState( { error } );
+	}
+
+	render() {
+		if ( this.state.error ) {
+			return (
+				<Notification type="error">
+					A problem occurred while rendering this content. Please report this as a bug.<br />
+					<code>{ this.state.error.toString() }</code>
+				</Notification>
+			);
+		}
+
+		return this.props.children;
+	}
+}
 
 function Content( props ) {
 	if ( ! props.useInterweave ) {
@@ -19,12 +47,14 @@ function Content( props ) {
 
 	return (
 		<div className="PostContent">
-			<Interweave
-				commonClass={ null }
-				content={ props.html }
-				matchers={ matchers }
-				tagName="fragment"
-			/>
+			<ErrorBoundary>
+				<Interweave
+					commonClass={ null }
+					content={ props.html }
+					matchers={ matchers }
+					tagName="fragment"
+				/>
+			</ErrorBoundary>
 		</div>
 	);
 }
