faf966734d3297638d372e6e0aa9a97a299c61f9
Ryan McCue <me@ryanmccue.info>
2018-09-14 12:07:29 +1000

    Render embeds in comments

--- inc/namespace.php
+++ inc/namespace.php
@@ -14,6 +14,11 @@ use WP_REST_Request;
 function adjust_default_filters() {
 	// Add make_clickable to posts
 	add_filter( 'the_content', 'make_clickable', 9 );
+
+	// Render embeds in comments
+	global $wp_embed;
+	add_filter( 'comment_text', [ $wp_embed, 'run_shortcode' ], 8 );
+	add_filter( 'comment_text', [ $wp_embed, 'autoembed' ], 8 );
 }
 
 /**
