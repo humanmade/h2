418fb2695db1cc5bd882dc32f32fe165e86a5922
Ryan McCue <me@ryanmccue.info>
2018-09-16 14:27:17 +1000

    Use Lorem Ipsum for dummy content

--- src/stories/stubs.js
+++ src/stories/stubs.js
@@ -1,14 +1,95 @@
+import outdent from 'outdent';
+
 const now = new Date().toISOString().replace( 'Z', '' );
+
+export const title = {
+	rendered: 'Tecum optime, deinde etiam cum mediocri amico.',
+};
+export const editableTitle = {
+	...title,
+	raw: 'Tecum optime, deinde etiam cum mediocri amico.',
+};
+
+export const content = {
+	rendered: outdent`
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Collige omnia, quae soletis: Praesidium amicorum. Si est nihil nisi corpus, summa erunt illa: valitudo, vacuitas doloris, pulchritudo, cetera. Duo Reges: constructio interrete. Itaque hic ipse iam pridem est reiectus; Nemo nostrum istius generis asotos iucunde putat vivere. </p>
+
+		<ul>
+			<li>Praetereo multos, in bis doctum hominem et suavem, Hieronymum, quem iam cur Peripateticum appellem nescio.</li>
+			<li>In schola desinis.</li>
+			<li>Itaque primos congressus copulationesque et consuetudinum instituendarum voluntates fieri propter voluptatem;</li>
+			<li>Neque enim civitas in seditione beata esse potest nec in discordia dominorum domus;</li>
+		</ul>
+
+
+		<ol>
+			<li>Quae si potest singula consolando levare, universa quo modo sustinebit?</li>
+			<li>Itaque nostrum est-quod nostrum dico, artis est-ad ea principia, quae accepimus.</li>
+			<li>Quid enim ab antiquis ex eo genere, quod ad disserendum valet, praetermissum est?</li>
+			<li>Ex eorum enim scriptis et institutis cum omnis doctrina liberalis, omnis historia.</li>
+			<li>Num igitur utiliorem tibi hunc Triarium putas esse posse, quam si tua sint Puteolis granaria?</li>
+		</ol>
+
+
+		<blockquote cite="http://loripsum.net">
+			Multosque etiam dolores curationis causa perferant, ut, si ipse usus membrorum non modo non maior, verum etiam minor futurus sit, eorum tamen species ad naturam revertatur?
+		</blockquote>
+
+
+		<p><i>Scrupulum, inquam, abeunti;</i> Nunc de hominis summo bono quaeritur; <code>Si id dicis, vicimus.</code> Plane idem, inquit, et maxima quidem, qua fieri nulla maior potest. Satis est tibi in te, satis in legibus, satis in mediocribus amicitiis praesidii. <code>Qui est in parvis malis.</code> Ut optime, secundum naturam affectum esse possit. </p>
+
+		<pre>Hanc initio institutionem confusam habet et incertam, ut
+		tantum modo se tueatur, qualecumque sit, sed nec quid sit
+		nec quid possit nec quid ipsius natura sit intellegit.
+
+		Si ad corpus pertinentibus, rationes tuas te video
+		compensare cum istis doloribus, non memoriam corpore
+		perceptarum voluptatum;
+		</pre>
+
+
+		<p><i>At enim hic etiam dolore.</i> Qua igitur re ab deo vincitur, si aeternitate non vincitur? Quid enim ab antiquis ex eo genere, quod ad disserendum valet, praetermissum est? <i>Beatus sibi videtur esse moriens.</i> Sint modo partes vitae beatae. Illis videtur, qui illud non dubitant bonum dicere -; An haec ab eo non dicuntur? </p>
+	`,
+};
+
+export const editableContent = {
+	...content,
+	raw: outdent`
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Collige omnia, quae soletis: Praesidium amicorum. Si est nihil nisi corpus, summa erunt illa: valitudo, vacuitas doloris, pulchritudo, cetera. Duo Reges: constructio interrete. Itaque hic ipse iam pridem est reiectus; Nemo nostrum istius generis asotos iucunde putat vivere.
+
+		-   Praetereo multos, in bis doctum hominem et suavem, Hieronymum, quem iam cur Peripateticum appellem nescio.
+		-   In schola desinis.
+		-   Itaque primos congressus copulationesque et consuetudinum instituendarum voluntates fieri propter voluptatem;
+		-   Neque enim civitas in seditione beata esse potest nec in discordia dominorum domus;
+
+		1.  Quae si potest singula consolando levare, universa quo modo sustinebit?
+		2.  Itaque nostrum est-quod nostrum dico, artis est-ad ea principia, quae accepimus.
+		3.  Quid enim ab antiquis ex eo genere, quod ad disserendum valet, praetermissum est?
+		4.  Ex eorum enim scriptis et institutis cum omnis doctrina liberalis, omnis historia.
+		5.  Num igitur utiliorem tibi hunc Triarium putas esse posse, quam si tua sint Puteolis granaria?
+
+		> Multosque etiam dolores curationis causa perferant, ut, si ipse usus membrorum non modo non maior, verum etiam minor futurus sit, eorum tamen species ad naturam revertatur?
+
+		*Scrupulum, inquam, abeunti;* Nunc de hominis summo bono quaeritur; \`Si id dicis, vicimus.\` Plane idem, inquit, et maxima quidem, qua fieri nulla maior potest. Satis est tibi in te, satis in legibus, satis in mediocribus amicitiis praesidii. \`Qui est in parvis malis.\` Ut optime, secundum naturam affectum esse possit.
+
+		Hanc initio institutionem confusam habet et incertam, ut
+		tantum modo se tueatur, qualecumque sit, sed nec quid sit
+		nec quid possit nec quid ipsius natura sit intellegit.
+
+		Si ad corpus pertinentibus, rationes tuas te video
+		compensare cum istis doloribus, non memoriam corpore
+		perceptarum voluptatum;
+
+		*At enim hic etiam dolore.* Qua igitur re ab deo vincitur, si aeternitate non vincitur? Quid enim ab antiquis ex eo genere, quod ad disserendum valet, praetermissum est? *Beatus sibi videtur esse moriens.* Sint modo partes vitae beatae. Illis videtur, qui illud non dubitant bonum dicere -; An haec ab eo non dicuntur?
+	`,
+};
 
 export const comment = {
 	id: 1,
 	post: 1,
 	date_gmt: now,
 	author: 1,
-	content: {
-		rendered: 'Heading up to Oregon for my brother and sister-in-law’s birthday celebrations! I’ll be out May 4-7!',
-		raw: 'awd',
-	},
+	content,
 };
 
 export const post = {
@@ -16,9 +97,9 @@ export const post = {
 	date_gmt: now,
 	author: 1,
 	categories: [],
-	link: 'http://example.com/2018/01/01/exploring/',
-	title: { rendered: 'Exploring the idea of Platform' },
-	content: { rendered: 'Heading up to Oregon for my brother and sister-in-law’s birthday celebrations! I’ll be out May 4-7!' },
+	link: 'http://example.com/2018/01/01/tecum-optime/',
+	title,
+	content,
 	related: {
 		comments: {
 			items: [],
