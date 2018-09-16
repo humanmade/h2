5dabe3ec2535524f9dbb833d85f8b97b9dddd79a
Ryan McCue <me@ryanmccue.info>
2018-09-16 14:49:11 +1000

    Rework header story

--- src/stories/header.js
+++ src/stories/header.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { storiesOf, action } from '@storybook/react';
 
-import Header from '../components/Header';
+import { user } from './stubs';
+import { apiResponse } from './util';
+import { Header } from '../components/Header';
 import Logo from '../components/Header/Logo';
+
+const defaultProps = {
+	currentUser: apiResponse( user ),
+	onLogOut: action( 'onLogOut' ),
+	onSearch: action( 'onSearch' ),
+	onShowChanges: action( 'onShowChanges' ),
+	onShowSuper: action( 'onShowSuper' ),
+	onWritePost: action( 'onWritePost' ),
+};
 
 storiesOf( 'Header', module )
 	.add( 'Header', () => (
-		<Header onWritePost={ () => {} } onWriteStatus={ () => {} }><Logo /></Header>
+		<Header
+			{ ...defaultProps }
+		/>
 	) )
 	.add( 'Logo', () => <Logo /> );
