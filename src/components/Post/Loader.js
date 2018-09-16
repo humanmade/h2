6729f0c331a8b353e07c65e4fb0bcf59df7a096c
Ryan McCue <me@ryanmccue.info>
2018-09-16 12:21:59 +1000

    Allow the loader to be responsive to width

--- src/components/Post/Loader.js
+++ src/components/Post/Loader.js
@@ -3,14 +3,19 @@ import ContentLoader from 'react-content-loader';
 
 import './Loader.css';
 
-export default function Loader() {
+export default function Loader( props ) {
+	const { width } = props;
+	const editButtonWidth = 48;
+	const replyButtonWidth = 63;
+
 	const contentOffset = 89;
 	const textLineHeight = 28;
 	return (
 		<ContentLoader
 			className="Post-Loader"
 			height={ contentOffset + 5 * textLineHeight }
-			width={ 740 }
+			width={ width }
+			style={ { width } }
 		>
 			{/* Avatar */}
 			<circle x="0" cx="30" cy="30" r="30" />
@@ -20,8 +25,22 @@ export default function Loader() {
 			<rect x="90" y="46" rx="3" ry="3" width="160" height="10.5" />
 
 			{/* Buttons */}
-			<rect x="622.5" y="12" rx="3" ry="3" width="48" height="30" />
-			<rect x="677.5" y="12" rx="3" ry="3" width="63" height="30" />
+			<rect
+				x={ width - replyButtonWidth - 10 - editButtonWidth }
+				y="12"
+				rx="3"
+				ry="3"
+				width={ editButtonWidth }
+				height="30"
+			/>
+			<rect
+				x={ width - replyButtonWidth }
+				y="12"
+				rx="3"
+				ry="3"
+				width={ replyButtonWidth }
+				height="30"
+			/>
 
 			{/* Text */}
 			<rect x="90" y={ contentOffset } rx="3" ry="3" width="625" height="13" />
@@ -30,4 +49,8 @@ export default function Loader() {
 			<rect x="90" y={ contentOffset + 3 * textLineHeight } rx="3" ry="3" width="610" height="13" />
 		</ContentLoader>
 	);
+};
+
+Loader.defaultProps = {
+	width: 740,
 };
