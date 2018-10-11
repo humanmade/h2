d71aae913e3f77646aad5f6801ee02d536d6f824
Ryan McCue <me@ryanmccue.info>
2018-10-11 13:17:45 +1000

    Add keyboard navigation for search results

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -9,52 +9,110 @@ import { withApiData } from '../with-api-data';
 
 import './SearchInput.css';
 
-const Results = props => {
-	console.log( props );
+class Results extends React.Component {
+	state = {
+		selected: -1,
+	}
 
-	const classes = [
-		'SearchInput__results',
-		props.visible && 'SearchInput__results--visible',
-	];
-	return (
-		<div className={ classes.filter( Boolean ).join( ' ' ) }>
-			{ props.term === '' ? (
-				<p>Start typing to search.</p>
-			) : props.results.isLoading ? (
-				<p>Loading results for “{ props.term }”</p>
-			) : props.results.error ? (
-				<p>Error while loading results. <span aria-label="Sorry." role="img">😰</span></p>
-			) : ( props.results.data && props.results.data.length > 0 ) ? (
-				<ul>
-					{ props.results.data.map( post => (
-						<li
-							key={ post.id }
-						>
-							<RelativeLink
-								className="SearchInput__result"
-								to={ post.link }
+	componentDidMount() {
+		this.keyHandler = e => {
+			const items = this.props.results && this.props.results.data;
+			if ( ! this.props.visible || ! items || ! items.length ) {
+				return;
+			}
+
+			const { selected } = this.state;
+			switch ( e.key ) {
+				case 'ArrowUp':
+					e.preventDefault();
+					this.setState( state => {
+						const nextSelection = Math.max( 0, selected - 1 );
+						return { selected: nextSelection };
+					} );
+					return;
+
+				case 'ArrowDown':
+					e.preventDefault();
+					this.setState( state => {
+						const nextSelection = Math.min( selected + 1, items.length - 1 );
+						return { selected: nextSelection };
+					} );
+					return;
+
+				case 'Enter': {
+					e.preventDefault();
+
+					const item = items[ selected ];
+					this.props.onSelect( item );
+					this.setState( { selected: -1 } );
+					return;
+				}
+
+				default:
+					// No-op
+					return;
+			}
+		};
+
+		window.addEventListener( 'keydown', this.keyHandler );
+	}
+
+	componentWillUnmount() {
+		if ( ! this.keyHandler ) {
+			return;
+		}
+
+		window.removeEventListener( 'keydown', this.keyHandler );
+	}
+
+	render() {
+		const { selected } = this.state;
+		const { results, term, visible } = this.props;
+
+		const classes = [
+			'SearchInput__results',
+			visible && 'SearchInput__results--visible',
+		];
+		return (
+			<div className={ classes.filter( Boolean ).join( ' ' ) }>
+				{ term === '' ? (
+					<p>Start typing to search.</p>
+				) : results.isLoading ? (
+					<p>Loading results for “{ term }”</p>
+				) : results.error ? (
+					<p>Error while loading results. <span aria-label="Sorry." role="img">😰</span></p>
+				) : ( results.data && results.data.length > 0 ) ? (
+					<ul>
+						{ results.data.map( ( post, index ) => (
+							<li
+								key={ post.id }
 							>
-								<p>
-									{ post.title.rendered }
-								</p>
-								<p>
-									<time
-										dateTime={ post.date + 'Z' }
-										title={ post.date + 'Z' }
-									>
-										<FormattedRelative value={ post.date + 'Z' } />
-									</time>
-								</p>
-							</RelativeLink>
-						</li>
-					) ) }
-				</ul>
-			) : (
-				<p>No results found.</p>
-			) }
-		</div>
-	);
-};
+								<RelativeLink
+									className={ `SearchInput__result ${ index === selected ? 'SearchInput__result--selected' : '' }` }
+									to={ post.link }
+								>
+									<p>
+										{ post.title.rendered }
+									</p>
+									<p>
+										<time
+											dateTime={ post.date + 'Z' }
+											title={ post.date + 'Z' }
+										>
+											<FormattedRelative value={ post.date + 'Z' } />
+										</time>
+									</p>
+								</RelativeLink>
+							</li>
+						) ) }
+					</ul>
+				) : (
+					<p>No results found.</p>
+				) }
+			</div>
+		);
+	}
+}
 
 const ConnectedResults = withApiData(
 	props => {
@@ -93,6 +151,15 @@ class SearchInput extends React.Component {
 		}, 100 );
 	}
 
+	onSelect = item => {
+		const path = item.link.replace( /^(?:\/\/|[^/]+)*\//, '/' );
+		this.props.history.push( path );
+
+		if ( this.inputEl ) {
+			this.inputEl.blur();
+		}
+	}
+
 	onSubmit = e => {
 		e.preventDefault();
 		this.props.onSearch( this.state.value );
@@ -112,6 +179,7 @@ class SearchInput extends React.Component {
 					<input
 						type="search"
 						placeholder="Search..."
+						ref={ ref => this.inputEl = ref }
 						value={ term }
 						onChange={ e => this.setState( { value: e.target.value } ) }
 						onFocus={ () => this.setState( { showSuggest: true } ) }
@@ -122,6 +190,7 @@ class SearchInput extends React.Component {
 				<ConnectedResults
 					term={ term }
 					visible={ this.state.showSuggest }
+					onSelect={ this.onSelect }
 				/>
 			</form>
 		);
