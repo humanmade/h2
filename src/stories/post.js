8eb58d73684ff259ed6736422470bfc7c90dbb4f
Ryan McCue <me@ryanmccue.info>
2019-08-12 12:48:00 +0100

    Switch to action addon

--- src/stories/post.js
+++ src/stories/post.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { storiesOf, action } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
 
 import { comment, editablePost, post, user } from './stubs';
 import { apiResponse } from './util';
