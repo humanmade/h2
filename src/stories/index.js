b8cd4750cdeaf948d1d10ac0fb0fb2b879630e6c
Ryan McCue <me@ryanmccue.info>
2019-08-12 16:21:56 +0100

    Convert avatar story to withStore

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 
+import withStore from './withStore';
 import { comment, post, user } from './stubs';
 import Avatar from '../components/Avatar';
 import Comment from '../components/Comment';
@@ -10,12 +11,6 @@ import WriteComment from '../components/Message/WriteComment';
 import '../hm-pattern-library/assets/styles/juniper.css';
 
 storiesOf( 'Components', module )
-	.add( 'Avatar', () => (
-		<Avatar
-			size={ 32 }
-			url="https://secure.gravatar.com/avatar/0ceb885cc3d306af93c9764b2936d618?s=300&d=mm&r=g"
-		/>
-	) )
 	.add( 'Comment', () => <Comment author={ user } comment={ comment } /> )
 	.add( 'WriteComment', () => (
 		<WriteComment
@@ -27,3 +22,12 @@ storiesOf( 'Components', module )
 			onCancel={ () => {} }
 		/>
 	) )
+
+storiesOf( 'Components' )
+	.addDecorator( withStore( {} ) )
+	.add( 'Avatar', () => (
+		<Avatar
+			size={ 32 }
+			url="https://secure.gravatar.com/avatar/0ceb885cc3d306af93c9764b2936d618?s=300&d=mm&r=g"
+		/>
+	) );
