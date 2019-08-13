0278d6861f89b0d9221f97e11269c9f1c380c21c
Ryan McCue <me@ryanmccue.info>
2019-08-13 17:22:19 +0100

    Remove top-level Redux store

--- src/stories/root-decorator.js
+++ src/stories/root-decorator.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { IntlProvider } from 'react-intl';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider as SlotFillProvider } from 'react-slot-fill';
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
 
-import reducers from '../reducers';
 import { Provider as RestApiProvider } from '../with-api-data';
 
 export default () => {
-	const store = createStore(
-		reducers,
-		applyMiddleware( thunk )
-	);
-
 	return story => {
 		const dummyFetch = ( url, ...args ) => {
 			console.log( url, ...args );
@@ -22,20 +13,18 @@ export default () => {
 		};
 
 		return (
-			<Provider store={ store }>
-				<SlotFillProvider>
-					<IntlProvider locale="en">
-						<RestApiProvider
-							fetch={ dummyFetch }
-							initialData={ null }
-						>
-							<Router>
-								{ story() }
-							</Router>
-						</RestApiProvider>
-					</IntlProvider>
-				</SlotFillProvider>
-			</Provider>
+			<SlotFillProvider>
+				<IntlProvider locale="en">
+					<RestApiProvider
+						fetch={ dummyFetch }
+						initialData={ null }
+					>
+						<Router>
+							{ story() }
+						</Router>
+					</RestApiProvider>
+				</IntlProvider>
+			</SlotFillProvider>
 		);
 	};
 };
