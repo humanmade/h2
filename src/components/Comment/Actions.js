4d4a192ae886478d78af7e8a96f3cb2d058b6011
Ryan McCue <me@ryanmccue.info>
2019-09-13 13:07:03 +0100

    Move comment actions into dropdown

--- src/components/Comment/Actions.js
+++ src/components/Comment/Actions.js
@@ -2,17 +2,38 @@ import React from 'react';
 import { Slot } from 'react-slot-fill';
 
 import Button from '../Button';
+import ButtonGroup from '../ButtonGroup';
+import Dropdown from '../Dropdown';
 
 export default function Actions( props ) {
 	const { fillProps, isEditing, onEdit, onReply } = props;
 
+	const renderItems = items => {
+		if ( ! items.length && isEditing ) {
+			return null;
+		}
+
+		return (
+			<Dropdown>
+				{ ! isEditing && (
+					<Button onClick={ onEdit }>Edit</Button>
+				) }
+
+				{ items }
+			</Dropdown>
+		);
+	}
+
 	return (
-		<div className="actions">
-			{ ! isEditing && (
-				<Button onClick={ onEdit }>Edit</Button>
-			) }
+		<ButtonGroup className="actions">
 			<Button onClick={ onReply }>Reply</Button>
 			<Slot name="Comment.actions" fillChildProps={ fillProps } />
-		</div>
+			<Slot
+				name="Comment.secondary_actions"
+				fillChildProps={ fillProps }
+			>
+				{ renderItems }
+			</Slot>
+		</ButtonGroup>
 	);
 }
