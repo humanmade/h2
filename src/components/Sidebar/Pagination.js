e943e4bc0358da26de5f88164721c5c95a81db60
Ryan McCue <me@ryanmccue.info>
2019-09-14 14:26:50 +0100

    Extend pagination for Sidebar Pagination

--- src/components/Sidebar/Pagination.js
+++ src/components/Sidebar/Pagination.js
@@ -7,21 +7,25 @@ import './Pagination.css';
 export default function SidebarPagination( props ) {
 	const { hasNext, hasPrevious, onNext, onPrevious } = props;
 	return (
-		<div className="Sidebar-Pagination">
+		<div className="Sidebar-Pagination pagination">
 			{ hasNext && (
 				<div className="Sidebar-Pagination__older">
 					<LinkButton
-						className="cta cta--small cta--arrow-left"
 						onClick={ onNext }
-					>Older</LinkButton>
+					>
+						<i className="icon icon--arrow-left icon--small icon--red" />
+						Older
+					</LinkButton>
 				</div>
 			) }
 			{ hasPrevious && (
 				<div className="Sidebar-Pagination__newer">
 					<LinkButton
-						className="cta cta--small cta--arrow"
 						onClick={ onPrevious }
-					>Newer</LinkButton>
+					>
+						Newer
+						<i className="icon icon--arrow-right icon--small icon--red" />
+					</LinkButton>
 				</div>
 			) }
 		</div>
