3f7167f42d4e6ee7e729ff165bc36f690b379c59
Ryan McCue <me@ryanmccue.info>
2019-09-15 14:02:47 +0100

    Properly convert search input stories

--- src/components/SearchInput.stories.js
+++ src/components/SearchInput.stories.js
@@ -6,63 +6,80 @@ import SearchInput, { Results } from './SearchInput';
 import { withCentering, withStore } from '../stories/decorators';
 import { post } from '../stories/stubs';
 
-storiesOf( 'Components/Search', module )
-	.addDecorator( withStore( {} ) )
-	.addDecorator( withCentering( { width: '50vw' } ) )
-	.add( 'Input', () => {
-		const ResultsComponent = props => (
-			<Results
-				{ ...props }
-				loading={ false }
-				posts={ [ post ] }
-			/>
-		);
-		return (
-			<SearchInput
-				value="value"
-				onSearch={ action( 'onSearch' ) }
-				resultsComponent={ ResultsComponent }
-			/>
-		);
-	} )
-	.add( 'Input (Showing Results)', () => {
-		const ResultsComponent = props => (
-			<Results
-				{ ...props }
-				loading={ false }
-				posts={ [ post ] }
-				visible
-			/>
-		);
-		return (
-			<SearchInput
-				value="value"
-				onSearch={ action( 'onSearch' ) }
-				resultsComponent={ ResultsComponent }
-			/>
-		);
-	} )
-	.add( 'Input (Loading)', () => {
-		const ResultsComponent = props => (
-			<Results
-				{ ...props }
-				loading={ true }
-				visible
-			/>
-		);
-		return (
-			<SearchInput
-				value="value"
-				onSearch={ action( 'onSearch' ) }
-				resultsComponent={ ResultsComponent }
-			/>
-		);
-	} )
-	.add( 'Results', () => (
+export default {
+	title: 'Search',
+	decorators: [
+		withStore( {} ),
+		withCentering( { width: '50vw' } ),
+	],
+}
+
+export const Input = () => {
+	const ResultsComponent = props => (
 		<Results
+			{ ...props }
 			loading={ false }
 			posts={ [ post ] }
-			term="lorem"
+		/>
+	);
+	return (
+		<SearchInput
+			value="value"
+			onSearch={ action( 'onSearch' ) }
+			resultsComponent={ ResultsComponent }
+		/>
+	);
+}
+
+export const ShowingResults = () => {
+	const ResultsComponent = props => (
+		<Results
+			{ ...props }
+			loading={ false }
+			posts={ [ post ] }
 			visible
 		/>
-	) );
+	);
+	return (
+		<SearchInput
+			value="value"
+			onSearch={ action( 'onSearch' ) }
+			resultsComponent={ ResultsComponent }
+		/>
+	);
+}
+ShowingResults.story = {
+	title: 'Input (Showing Results)',
+};
+
+export const Loading = () => {
+	const ResultsComponent = props => (
+		<Results
+			{ ...props }
+			loading={ true }
+			visible
+		/>
+	);
+	return (
+		<SearchInput
+			value="value"
+			onSearch={ action( 'onSearch' ) }
+			resultsComponent={ ResultsComponent }
+		/>
+	);
+}
+Loading.story = {
+	title: 'Input (Loading)',
+};
+
+export const SearchResults = () => (
+	<Results
+		loading={ false }
+		posts={ [ post ] }
+		term="lorem"
+		visible
+	/>
+);
+SearchResults.story = {
+	title: 'Results',
+};
