72ad56040f6da90dc4ceb66f8ad7445e88e2e1be
Ryan McCue <me@ryanmccue.info>
2019-09-15 13:49:38 +0100

    Add stories for Pagination

---
+++ src/components/Pagination.stories.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Pagination from './Pagination';
+
+import { withCentering } from '../stories/decorators';
+
+export default {
+	title: 'Pagination',
+	decorators: [
+		withCentering( {
+			minWidth: '30vw',
+		} ),
+	],
+}
+
+const commonProps = {
+	hasNext: true,
+	params: {
+		page: 1,
+	},
+	path: '/',
+};
+
+export const Older = () => (
+	<Pagination
+		{ ...commonProps }
+	/>
+);
+export const Newer = () => (
+	<Pagination
+		{ ...commonProps }
+		hasNext={ false }
+		params={ { page: 2 } }
+	/>
+);
+export const Both = () => (
+	<Pagination
+		{ ...commonProps }
+		params={ { page: 2 } }
+	/>
+);
+export const Neither = () => (
+	<Pagination
+		{ ...commonProps }
+		hasNext={ false }
+	/>
+);
