ccc3b8daa7dfd8ce1b7b1eb1be94449817810e28
Ryan McCue <me@ryanmccue.info>
2019-09-15 14:16:48 +0100

    Split avatar stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,8 +2,7 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 
 import { htmlTester } from './stubs';
-import { withCentering, withPadding, withStore } from './decorators';
-import Avatar from '../components/Avatar';
+import { withPadding } from './decorators';
 import { Content } from '../components/Message/Content';
 
 import '../hm-pattern-library/assets/styles/juniper.css';
@@ -18,15 +17,5 @@ storiesOf( 'Base Styles' )
 	.add( 'Content', () => (
 		<Content
 			html={ htmlTester }
-		/>
-	) );
-
-storiesOf( 'Components', module )
-	.addDecorator( withStore( {} ) )
-	.addDecorator( withCentering() )
-	.add( 'Avatar', () => (
-		<Avatar
-			size={ 32 }
-			url="https://secure.gravatar.com/avatar/0ceb885cc3d306af93c9764b2936d618?s=300&d=mm&r=g"
 		/>
 	) );
