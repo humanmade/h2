c073b1fd24911a8e1bd1d2cd269d9bb9c3a0535d
Ryan McCue <me@ryanmccue.info>
2019-09-15 14:06:18 +0100

    Move DropUpload stories to separate file

---
+++ src/components/DropUpload.stories.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import { withCentering, withStore } from '../stories/decorators';
+import DropUpload from '../components/DropUpload';
+
+export default {
+	title: 'Editor/Upload',
+	decorators: [
+		withStore(),
+		withCentering( { minWidth: '30vw' } ),
+	],
+}
+
+export const Normal = () => (
+	<DropUpload
+		files={ [] }
+		onCancel={ action( 'onCancel' ) }
+		onUpload={ action( 'onUpload' ) }
+	>
+		<p style={ { background: '#eee' } }>DropUpload children.</p>
+	</DropUpload>
+);
+
+export const Progress = () => (
+	<DropUpload
+		files={ [ { name: 'filename.ext' } ] }
+		onCancel={ action( 'onCancel' ) }
+		onUpload={ action( 'onUpload' ) }
+	>
+		<p style={ { background: '#eee' } }>DropUpload children.</p>
+	</DropUpload>
+);
+Progress.story = {
+	title: 'In Progress',
+};
+
+export const ProgressMulti = () => (
+	<DropUpload
+		files={ [ { name: 'filename.ext' }, { name: 'other.ext' } ] }
+		onCancel={ action( 'onCancel' ) }
+		onUpload={ action( 'onUpload' ) }
+	>
+		<p style={ { background: '#eee' } }>DropUpload children.</p>
+	</DropUpload>
+);
+ProgressMulti.story = {
+	title: 'In Progress (Multiple)',
+};
--- src/stories/editor.js
+++ src/stories/editor.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 
-import { withCentering, withPadding, withStore } from './decorators';
+import { withPadding, withStore } from './decorators';
 import { users } from './stubs';
 import Editor from '../components/Editor';
-import DropUpload from '../components/DropUpload';
 
 storiesOf( 'Editor', module )
 	.addDecorator( withStore( {} ) )
@@ -15,35 +14,4 @@ storiesOf( 'Editor', module )
 			onSubmit={ action( 'onSubmit' ) }
 			users={ users }
 		/>
-	) );
-
-storiesOf( 'Editor/Upload', module )
-	.addDecorator( withStore( {} ) )
-	.addDecorator( withCentering( { minWidth: '30vw' } ) )
-	.add( 'Normal', () => (
-		<DropUpload
-			files={ [] }
-			onCancel={ action( 'onCancel' ) }
-			onUpload={ action( 'onUpload' ) }
-		>
-			<p style={ { background: '#eee' } }>DropUpload children.</p>
-		</DropUpload>
-	) )
-	.add( 'In Progress', () => (
-		<DropUpload
-			files={ [ { name: 'filename.ext' } ] }
-			onCancel={ action( 'onCancel' ) }
-			onUpload={ action( 'onUpload' ) }
-		>
-			<p style={ { background: '#eee' } }>DropUpload children.</p>
-		</DropUpload>
-	) )
-	.add( 'In Progress (Multiple)', () => (
-		<DropUpload
-			files={ [ { name: 'filename.ext' }, { name: 'other.ext' } ] }
-			onCancel={ action( 'onCancel' ) }
-			onUpload={ action( 'onUpload' ) }
-		>
-			<p style={ { background: '#eee' } }>DropUpload children.</p>
-		</DropUpload>
 	) );
