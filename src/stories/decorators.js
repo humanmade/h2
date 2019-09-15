a1b0350830db994a3bffa5873dbd54fc0dd435a4
Ryan McCue <me@ryanmccue.info>
2019-09-15 11:43:19 +0100

    Add withCentering decorator

--- src/stories/decorators.js
+++ src/stories/decorators.js
@@ -27,20 +27,53 @@ export function withStore( state, actions = [] ) {
 	return withRedux( addons )( settings );
 }
 
+const checkerboardStyle = {
+	backgroundImage: `
+		linear-gradient(45deg, #f3f3f3 25%, transparent 25%),
+		linear-gradient(-45deg, #f3f3f3 25%, transparent 25%),
+		linear-gradient(45deg, transparent 75%, #f3f3f3 75%),
+		linear-gradient(-45deg, transparent 75%, #f3f3f3 75%)
+	`,
+	backgroundSize: '20px 20px',
+	backgroundPosition: '0 0, 0 10px, 10px -10px, -10px 0px',
+};
+
 export const withPadding = story => {
 	const style = {
 		padding: '40px',
-		backgroundImage: `
-			linear-gradient(45deg, #f3f3f3 25%, transparent 25%),
-			linear-gradient(-45deg, #f3f3f3 25%, transparent 25%),
-			linear-gradient(45deg, transparent 75%, #f3f3f3 75%),
-			linear-gradient(-45deg, transparent 75%, #f3f3f3 75%)
-		`,
-		backgroundSize: '20px 20px',
-		backgroundPosition: '0 0, 0 10px, 10px -10px, -10px 0px',
+		...checkerboardStyle,
 	};
 	const innerStyle = {
 		background: '#fff',
+	};
+
+	return (
+		<div style={ style }>
+			<div style={ innerStyle }>
+				{ story() }
+			</div>
+		</div>
+	);
+}
+
+export const withCentering = ( extraStyle = {} ) => story => {
+	const style = {
+		position: 'absolute',
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0,
+
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+
+		...checkerboardStyle,
+	};
+	const innerStyle = {
+		background: '#fff',
+		position: 'relative',
+		...extraStyle,
 	};
 
 	return (
