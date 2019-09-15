bc3b296ccc7b175bcffbd64e077fad327db8d6ee
Ryan McCue <me@ryanmccue.info>
2019-09-15 11:16:57 +0100

    Add viewport addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,5 +2,6 @@ import addons from '@storybook/addons'
 import registerRedux from 'addon-redux/register'
 
 import '@storybook/addon-actions/register';
+import '@storybook/addon-viewport/register';
 
 registerRedux( addons );
