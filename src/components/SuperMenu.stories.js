be6195027ab5f25422b50768f89292f43a6c256a
Ryan McCue <me@ryanmccue.info>
2019-09-15 13:31:52 +0100

    Add SuperMenu stories

---
+++ src/components/SuperMenu.stories.js
@@ -0,0 +1,48 @@
+import { action } from '@storybook/addon-actions';
+import React from 'react';
+import { SuperMenu } from './SuperMenu';
+
+import { withStore } from '../stories/decorators';
+
+export default {
+	title: 'Super Menu',
+	decorators: [
+		withStore(),
+	],
+};
+
+const sites = [
+	{
+		id: 1,
+		url: 'https://example.com/updates/',
+		name: 'Updates',
+	},
+	{
+		id: 2,
+		url: 'https://example.com/dev/',
+		name: 'Dev',
+	},
+];
+
+const commonProps = {
+	categories: [],
+	sites: [],
+	visible: true,
+	onClose: action( 'onClose' ),
+};
+
+export const Empty = () => (
+	<SuperMenu
+		{ ...commonProps }
+	/>
+);
+
+export const Sites = () => (
+	<SuperMenu
+		{ ...commonProps }
+		sites={ { data: sites } }
+	/>
+);
+Sites.story = {
+	name: 'With Sites',
+};
