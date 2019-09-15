02d53bd8f124ac87af925ebf2fd7dde58a66f318
Ryan McCue <me@ryanmccue.info>
2019-09-15 15:34:31 +0100

    Split button stories up

---
+++ src/components/ButtonGroup.stories.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+
+import Button from './Button';
+import ButtonGroup from './ButtonGroup';
+import { withCentering } from '../stories/decorators';
+
+export default {
+	title: 'Components/Form/Button/Group',
+	decorators: [
+		withCentering(),
+	],
+}
+
+const buttonTypes = [
+	'primary',
+	'secondary',
+	'tertiary',
+	'inverted',
+];
+export const All = () => (
+	<Fragment>
+		{ buttonTypes.map( type => (
+			<p key={ type }>
+				<ButtonGroup>
+					<Button type={ type }>First</Button>
+					<Button type={ type }>Second</Button>
+					<Button type={ type }>Third</Button>
+					<Button type={ type }>Forth</Button>
+				</ButtonGroup>
+			</p>
+		) ) }
+	</Fragment>
+);
