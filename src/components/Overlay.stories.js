2c990a62b99cff7f69b7a322c2eaa637966ddc96
Ryan McCue <me@ryanmccue.info>
2019-09-19 13:00:26 +0100

    Remove unneeded decorator from overlay

--- src/components/Overlay.stories.js
+++ src/components/Overlay.stories.js
@@ -2,13 +2,9 @@ import { action } from '@storybook/addon-actions';
 import React, { Fragment } from 'react';
 
 import Overlay, { OverlayContainer } from './Overlay';
-import { withCentering } from '../stories/decorators';
 
 export default {
 	title: 'Components|Overlay',
-	decorators: [
-		withCentering(),
-	],
 }
 
 export const Basic = () => (
