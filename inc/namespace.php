f1371e23f4ef35a5f5f357910c4014dcbe6e6f85
Joe Hoyle <joehoyle@gmail.com>
2019-10-03 19:32:43 +0200

    Bring back preload data, turns out this is used.

--- inc/namespace.php
+++ inc/namespace.php
@@ -65,6 +65,9 @@ function get_script_data() {
 	$preload = [
 		'/wp/v2/users/me',
 		'/wp/v2/users?per_page=100',
+		'/h2/v1/site-switcher/sites',
+		'/h2/v1/widgets?sidebar=sidebar',
+		'/wp/v2/categories?per_page=100',
 	];
 
 	$data = [
