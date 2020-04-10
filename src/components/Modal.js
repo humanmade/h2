82097f4eb24ef5ea5b7c8fb7f2bf342833fe680d
Shady Sharaf <shady@sharaf.me>
2020-04-10 16:21:47 +0200

    Allow using escape button to dismiss modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Overlay from './Overlay';
 import TitleBar from './Sidebar/TitleBar';
@@ -7,6 +7,14 @@ import './Modal.css';
 
 export default function Modal( props ) {
 	const { children, title, className, onDismiss } = props;
+	const onKeyUp = e => e.key === 'Escape' && onDismiss();
+
+	// Allow using ESCAPE button to dismiss the modal
+	useEffect( () => {
+		document.addEventListener( 'keyup', onKeyUp );
+
+		return () => document.removeEventListener( 'keyup', onKeyUp );
+	}, [] );
 
 	return (
 		<div className={ `Modal ${ className }` }>
