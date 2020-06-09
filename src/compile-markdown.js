277c1205b6518328ef8f8e1ef107d19e3d007892
K Adam White <kadamwhite@users.noreply.github.com>
2020-06-09 15:20:45 -0400

    Remove custom checkbox handling in compile-markdown module

--- src/compile-markdown.js
+++ src/compile-markdown.js
@@ -1,7 +1,5 @@
 import marked from 'marked';
 
-const TASK_LIST_TOKEN = '<!-- H2_TASKLIST_ITEM -->';
-
 class CustomRender extends marked.Renderer {
 	link( href, title, text ) {
 		if ( href === text && ! title ) {
@@ -10,24 +8,6 @@ class CustomRender extends marked.Renderer {
 		}
 
 		return super.link( href, title, text );
-	}
-
-	list( body, ordered ) {
-		if ( body.indexOf( TASK_LIST_TOKEN ) >= 0 ) {
-			return '<ul class="Tasklist">' + body.replace( new RegExp( TASK_LIST_TOKEN, 'g' ), '' ) + '</ul>';
-		}
-
-		return super.list( body, ordered );
-	}
-
-	listitem( text ) {
-		const listMatch = text.match( /^\[(x| )] ?(.+)/i );
-		if ( listMatch ) {
-			const checked = listMatch[1] !== ' ';
-			return `<li class="Tasklist-Item"${ checked ? ' data-checked' : '' }>${ listMatch[2] }</li>\n` + TASK_LIST_TOKEN;
-		}
-
-		return super.listitem( text );
 	}
 }
 
