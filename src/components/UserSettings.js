395e71e0600a1630b3ecb05088ce60c776412021
K Adam White <kadamwhite@users.noreply.github.com>
2020-10-29 07:21:45 -0400

    Grudgingly convert functional component to class component

--- src/components/UserSettings.js
+++ src/components/UserSettings.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import { withCurrentUser } from '../hocs';
 
@@ -17,43 +17,47 @@ const viewOptions = [
 	},
 ];
 
-export const UserSettings = ( { currentUser, onUpdateCurrentUser } ) => {
-	if ( ! currentUser ) {
-		return null;
+export class UserSettings extends PureComponent {
+	render() {
+		const { currentUser, onUpdateCurrentUser } = this.props;
+
+		if ( ! currentUser ) {
+			return null;
+		}
+
+		const viewPreference = currentUser.meta.h2_view_preference;
+
+		const onUpdateViewPreference = event => {
+			const value = event.target.value;
+			onUpdateCurrentUser( {
+				meta: {
+					h2_view_preference: value,
+				},
+			} );
+		};
+
+		return (
+			<div className="UserSettings">
+				<h3>Settings</h3>
+				<label>
+					I prefer archive pages to display
+					<select
+						value={ viewPreference }
+						onChange={ onUpdateViewPreference }
+					>
+						{ viewOptions.map( ( { value, label } ) => (
+							<option
+								key={ value }
+								value={ value }
+							>
+								{ label }
+							</option>
+						) ) }
+					</select>
+				</label>
+			</div>
+		);
 	}
-
-	const viewPreference = currentUser.meta.h2_view_preference;
-
-	const onUpdateViewPreference = event => {
-		const value = event.target.value;
-		onUpdateCurrentUser( {
-			meta: {
-				h2_view_preference: value,
-			},
-		} );
-	};
-
-	return (
-		<div className="UserSettings">
-			<h3>Settings</h3>
-			<label>
-				I prefer archive pages to display
-				<select
-					value={ viewPreference }
-					onChange={ onUpdateViewPreference }
-				>
-					{ viewOptions.map( ( { value, label } ) => (
-						<option
-							key={ value }
-							value={ value }
-						>
-							{ label }
-						</option>
-					) ) }
-				</select>
-			</label>
-		</div>
-	);
-};
+}
 
 export default withCurrentUser( UserSettings );
