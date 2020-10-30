8f87493aae910ee5a80c4f3032ed850816290df9
K Adam White <kadamwhite@users.noreply.github.com>
2020-10-30 12:23:47 -0400

    Prevent SVG loading indicators from expanding page width
    Fixes #387
    
    I tracked this down by pausing JS script execution while the page was
    loading in a narrow viewport, which let me see that the loading elements
    were displaying at a wide fixed width that exceeded the dimensions of
    the viewport. Wrapping mobile loaders in a div that cannot exceed the
    page dimensions appears to fix the issue in my browser; the width can
    be adjusted if, after deploy, it still doesn't look right on mobile.

--- src/components/Comment/Loader.js
+++ src/components/Comment/Loader.js
@@ -29,27 +29,29 @@ export default class CommentLoader extends React.Component {
 		if ( this.state.isMobile ) {
 			contentOffset = 42;
 			return (
-				<ContentLoader
-					className="Comment-Loader Comment-Loader--mobile"
-					height={ contentOffset + 5 * textLineHeight }
-					width={ width }
-					style={ { width } }
-				>
-					{ /* Avatar */ }
-					<circle cx="21" cy="12" r="12" />
+				<div class="Loader-Wrapper">
+					<ContentLoader
+						className="Comment-Loader Comment-Loader--mobile"
+						height={ contentOffset + 5 * textLineHeight }
+						width={ width }
+						style={ { width } }
+					>
+						{ /* Avatar */ }
+						<circle cx="21" cy="12" r="12" />
 
-					{ /* Timeline */ }
-					<rect x="20" width="3" height={ contentOffset + 5 * textLineHeight } />
+						{ /* Timeline */ }
+						<rect x="20" width="3" height={ contentOffset + 5 * textLineHeight } />
 
-					{ /* Title */ }
-					<rect x="40" y="4" rx="4" ry="4" width="240" height="16" />
+						{ /* Title */ }
+						<rect x="40" y="4" rx="4" ry="4" width="240" height="16" />
 
-					{ /* Text */ }
-					<rect x="40" y={ contentOffset } rx="3" ry="3" width="325" height="13" />
-					<rect x="40" y={ contentOffset + textLineHeight } rx="3" ry="3" width="313" height="13" />
-					<rect x="40" y={ contentOffset + 2 * textLineHeight } rx="3" ry="3" width="280" height="13" />
-					<rect x="40" y={ contentOffset + 3 * textLineHeight } rx="3" ry="3" width="310" height="13" />
-				</ContentLoader>
+						{ /* Text */ }
+						<rect x="40" y={ contentOffset } rx="3" ry="3" width="325" height="13" />
+						<rect x="40" y={ contentOffset + textLineHeight } rx="3" ry="3" width="313" height="13" />
+						<rect x="40" y={ contentOffset + 2 * textLineHeight } rx="3" ry="3" width="280" height="13" />
+						<rect x="40" y={ contentOffset + 3 * textLineHeight } rx="3" ry="3" width="310" height="13" />
+					</ContentLoader>
+				</div>
 			);
 		}
 
