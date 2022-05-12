43e7bbe7a4ab735f8450431e08963c250352001e
K. Adam White <kadamwhite@users.noreply.github.com>
2022-05-12 10:24:47 -0400

    Use normal fetch, not posts handler, for remote preview

--- src/components/RemotePreview.js
+++ src/components/RemotePreview.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import compileMarkdown from '../compile-markdown';
-import { posts } from '../types';
 
 import MessageContent from './Message/Content';
 import Notification from './Notification';
@@ -33,14 +32,15 @@ export default class RemotePreview extends React.Component {
 			type: this.props.type,
 		};
 
-		posts.fetch( `${ window.wpApiSettings.root }h2/v1/preview`, {}, {
+		fetch( `${ window.wpApiSettings.root }h2/v1/preview`, {
+			method: 'POST',
+			credentials: 'same-origin',
 			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
+			  'Content-Type': 'application/json',
 			},
 			body: JSON.stringify( body ),
-			method: 'POST',
 		} )
+			.then( response => response.json() )
 			.then( response => {
 				this.setState( {
 					compiledPreview: response.html,
