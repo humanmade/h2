6314771ced6f217a4c26b49fddf3db74c83358bb
K. Adam White <kadamwhite@users.noreply.github.com>
2022-05-18 14:58:30 -0400

    Move optimization back to top

--- .config/webpack.config.prod.js
+++ .config/webpack.config.prod.js
@@ -3,6 +3,9 @@ const { helpers, presets, plugins } = require( '@humanmade/webpack-helpers' );
 const { filePath } = helpers;
 
 module.exports = presets.production( {
+	optimization: {
+		usedExports: true,
+	},
 	entry: {
 		h2: filePath( 'src/index.js' ),
 		'editor-style': filePath( 'src/editor-style.scss' ),
@@ -10,9 +13,6 @@ module.exports = presets.production( {
 	output: {
 		// Asset Loader understands filenames are already hashed in this format.
 		filename: '[name].[contenthash:16].js',
-	},
-	optimization: {
-		usedExports: true,
 	},
     plugins: [
         plugins.clean(),
