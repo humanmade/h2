532a0bcc7cf7eae8c23f99135653059452133a48
Ryan McCue <me@ryanmccue.info>
2025-10-26 19:01:42 +0100

    Convert SuperMenu to Tailwind

--- src/components/SuperMenu.js
+++ src/components/SuperMenu.js
@@ -9,8 +9,6 @@ import Link from './Link';
 import Overlay from './Overlay';
 import SearchInput from './SearchInput';
 
-import './SuperMenu.css';
-
 const Category = ( { all, category } ) => {
 	const childCategories = all.filter( cat => cat.parent === category.id );
 
@@ -31,7 +29,7 @@ const Category = ( { all, category } ) => {
 };
 
 const CategoryList = ( { all, categories } ) => (
-	<ul>
+	<ul className="list-none p-0 m-0 mb-6 [&_ul]:ml-4 [&_ul]:mb-0">
 		{ categories && categories.map( category => (
 			<Category
 				key={ category.id }
@@ -49,7 +47,8 @@ export class SuperMenu extends React.Component {
 
 		const classes = [
 			'SuperMenu',
-			visible && 'SuperMenu--visible',
+			'bg-hm-light-grey absolute w-[300px] h-screen overflow-auto z-30 p-4 px-5 shadow-[0_1px_3px_rgba(0,0,0,0.15)] transition-transform duration-150 flex flex-col',
+			visible ? 'SuperMenu--visible translate-x-0' : '-translate-x-[300px]',
 		];
 
 		const newChanges = this.props.currentUser ? getChangesForUser( this.props.currentUser ) : [];
@@ -70,10 +69,10 @@ export class SuperMenu extends React.Component {
 					/>
 				) }
 
-				<header>
+				<header className="-mx-5 -mt-4 mb-4 flex-shrink-0 flex items-center justify-between bg-brand pr-4 text-white [&_.hm-logo:hover]:opacity-100">
 					<Logo />
 					<button
-						className="SuperMenu--closer"
+						className="SuperMenu--closer bg-transparent border-none p-0 m-0 cursor-pointer"
 						type="button"
 						onClick={ this.props.onClose }
 					>
@@ -81,14 +80,16 @@ export class SuperMenu extends React.Component {
 					</button>
 				</header>
 
-				<div className="SuperMenu__content">
+				<div className="SuperMenu__content overflow-auto [&_a]:text-black [&_a:link]:text-black [&_a:visited]:text-black">
 					<SearchInput
+						className="mb-4 min-[600px]:hidden"
+						small
 						onSearch={ this.props.onSearch }
 					/>
 
-					<h2 className="screen-reader-text">Navigation</h2>
+					<h2 className="screen-reader-text text-base m-0 mb-4">Navigation</h2>
 
-					<ul>
+					<ul className="list-none p-0 mb-6">
 						<li><Link href={ window.H2Data.site.home }>All Posts</Link></li>
 						<li><a href={ `${ window.H2Data.site.home }/wp-admin/` }>Dashboard</a></li>
 					</ul>
@@ -102,7 +103,7 @@ export class SuperMenu extends React.Component {
 
 					{ categories.data && (
 						<React.Fragment>
-							<h3>Categories</h3>
+							<h3 className="text-hm-warm-grey uppercase text-base mt-3 mb-1">Categories</h3>
 							<CategoryList
 								all={ categories.data }
 								categories={ categories.data.filter( cat => cat.parent === 0 ) }
@@ -112,8 +113,8 @@ export class SuperMenu extends React.Component {
 
 					{ sites ? (
 						<React.Fragment>
-							<h3>Sites</h3>
-							<ul>
+							<h3 className="text-hm-warm-grey uppercase text-base mt-3 mb-1">Sites</h3>
+							<ul className="list-none p-0 m-0 mb-6">
 								{ sites.map( site => (
 									<li key={ site.id }>
 										<a href={ site.url }>
