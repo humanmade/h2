1c43f6a21260a3c678032080fb17a851742bea00
K. Adam White <kadamwhite@users.noreply.github.com>
2026-01-12 09:43:30 -0500

    Add WIP h2o base FSE theme

---
+++ patterns/hidden-404.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: 404
+ * Slug: h2o/404
+ * Categories: text
+ * inserter: no
+ *
+ * @package h2o
+ * @since 1.0.0
+ */
+
+?>
+<!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
+<h2 class="has-text-align-center has-x-large-font-size"><?php esc_html_e( 'Page not found', 'h2o' ); ?></h2>
+<!-- /wp:heading -->
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'h2o' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search"} /-->
+<!-- /wp:group -->
