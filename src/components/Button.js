5c2bbe83013ca1168e044a2877ce822b51ac11c6
Ryan McCue <me@ryanmccue.info>
2026-03-05 17:40:35 +0000

    Convert buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,27 +1,63 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import './Button.css';
+const TYPE_CLASSES = {
+	primary: [
+		'border-hm-vibrant-blue bg-hm-vibrant-blue text-white',
+		'hover:bg-white hover:text-hm-vibrant-blue',
+		'focus:bg-white focus:text-hm-vibrant-blue',
+		'disabled:hover:bg-hm-vibrant-blue disabled:hover:text-white',
+	].join( ' ' ),
+	secondary: [
+		'border-hm-vibrant-blue bg-white text-hm-vibrant-blue',
+		'hover:bg-hm-vibrant-blue hover:text-white',
+		'focus:bg-hm-vibrant-blue focus:text-white',
+		'disabled:hover:bg-hm-vibrant-blue disabled:hover:text-white',
+	].join( ' ' ),
+	tertiary: [
+		'border-hm-warm-grey bg-transparent text-hm-warm-grey',
+		'hover:bg-hm-warm-grey hover:text-white',
+		'focus:bg-hm-warm-grey focus:text-white',
+		'disabled:hover:bg-transparent disabled:hover:text-hm-warm-grey',
+	].join( ' ' ),
+	inverted: [
+		'border-white bg-transparent text-white',
+		'hover:bg-white hover:text-hm-vibrant-blue hover:border-hm-vibrant-blue',
+		'focus:bg-white focus:text-hm-vibrant-blue focus:border-hm-vibrant-blue',
+	].join( ' ' ),
+};
+
+const SIZE_CLASSES = {
+	regular: 'text-lg py-[calc(0.417rem+1px)] px-[15px]',
+	small: 'btn--small text-base border px-[7.5px] py-0',
+};
+
+const BASE_CLASSES = [
+	// Back-compat:
+	'btn',
+
+	'font-sans font-light',
+	'border border-solid rounded',
+	'inline-block text-center align-middle cursor-pointer',
+	'w-auto h-auto',
+	'shadow-none no-underline',
+	'transition-[background,border-color] duration-200 ease-in-out',
+	'mb-[0.833rem] mr-[7.5px]',
+	'focus:outline-none',
+	'disabled:cursor-default disabled:opacity-30',
+].join( ' ' );
 
 export default function Button( props ) {
-
-	let classes = [ 'btn' ];
-
-	if ( props.type ) {
-		classes.push( 'btn--' + props.type );
-	}
-
-	if ( props.size ) {
-		classes.push( 'btn--' + props.size );
-	}
-
-	if ( props.className ) {
-		classes.push( props.className );
-	}
+	const classes = [
+		BASE_CLASSES,
+		TYPE_CLASSES[ props.type ] || TYPE_CLASSES.secondary,
+		SIZE_CLASSES[ props.size ] || SIZE_CLASSES.small,
+		props.className,
+	].filter( Boolean ).join( ' ' );
 
 	return (
 		<button
-			className={ classes.join( ' ' ) }
+			className={ classes }
 			disabled={ props.disabled }
 			type={ props.submit ? 'submit' : 'button' }
 			onClick={ props.onClick || undefined }
