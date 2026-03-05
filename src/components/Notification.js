bc0d581e87d242109a3fa8b6efdce0d5220e82c3
Ryan McCue <me@ryanmccue.info>
2026-03-05 18:05:07 +0000

    Switch notification over

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,17 +1,31 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import './Notification.css';
-
 export const TYPES = {
 	STATUS: 'status',
 	ALERT: 'alert',
 	ERROR: 'error',
 };
 
+const BASE_CLASSES = [
+	// Back-compat:
+	'Notification',
+
+	'rounded mt-0 p-[1.1em] text-[0.777777778em]',
+	'border border-solid border-current',
+].join( ' ' );
+
+const TYPE_CLASSES = {
+	status: 'bg-[#ECF6FA] text-[#009ACE]',
+	alert: 'bg-[#FFFAE2] text-[#DB7E26]',
+	error: 'bg-[#FFF2F4] text-[#FF001F]',
+};
+
 export default function Notification( props ) {
+	const classes = [ BASE_CLASSES, TYPE_CLASSES[ props.type ] ].join( ' ' );
+
 	return (
-		<p className={ `Notification Notification--${ props.type }` }>
+		<p className={ classes }>
 			{ props.children }
 		</p>
 	);
